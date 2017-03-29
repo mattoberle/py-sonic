ee8dd3b48ee47833dd41344fcbcf4181e00dfa5f
Matt Oberle <matt.r.oberle@gmail.com>
2017-03-28 23:09:32 -0400

    uses setuptools.setup for `develop` support

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with py-sonic.  If not, see <http://www.gnu.org/licenses/>
 """
 
-from distutils.core import setup
+from setuptools import setup
 from libsonic import __version__ as version
 
 setup(name='py-sonic',
